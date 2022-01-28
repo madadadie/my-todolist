a9e712fba2e2b1d6564db3accb1528d54019eb66
madadadie <danielledadie05@gmail.com>
2022-01-28 15:03:57 +0100

    init todolist component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import TodoList from './components/TodoList'
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <p>Merci react</p>
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
 const TodoList = props => {
     return (
         <div>
-            
+            <h3>What are your daily tasks ?</h3>
+            <div class="form-group">
+                <input placeholder='Add task...' type='text' className='todo-input'/>
+            </div>
+
+          <button className='todo-button'>
+            ADD
+          </button>
         </div>
     );
 };
